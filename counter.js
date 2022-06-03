1faee5fed0284e582349ffbe9504eb6d76c382ae
Aaofash <102664550+Aaofash@users.noreply.github.com>
2022-06-03 17:38:26 -0500

    intro

---
+++ counter.js
@@ -0,0 +1,17 @@
+const counter = document.getElementById('counter');
+const buttons = document.querySelectorAll('.btn');
+let i = '0';
+buttons.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        const a = e.currentTarget.classList;
+        
+        if (a.contains('increase')){
+            i++;
+        }else if (a.contains('decrease')){
+            i--;
+        }else {
+            i = 0;
+        }
+        counter.textContent = i;
+    })
+})
